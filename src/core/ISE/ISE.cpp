a39533b2837f1ec258b58fa3f8b297fbf593ad11
Wesley-W <wesley.walt@gmail.com>
2015-01-28 15:33:24 -0600

    I think this right..
    I made stuff..

--- src/core/ISE/ISE.cpp
+++ src/core/ISE/ISE.cpp
@@ -11,51 +11,97 @@
 #include "qpixmap.h"
 #include "qdir.h"
 #include "qgraphicsitem.h"
+#include "qlayout.h"
+#include "qlabel.h"
+#include "qtoolbutton.h"
+
 
 using namespace GameViewer_space; // Ask me about this
 ISEBegin
-  ISE::ISE()
+	ISE::ISE()
 {
-
+	firstScore = 0;
+	secondScore = 0;
 }
 
 ISE::~ISE()
 {
+
 }
 
 void setBackground(QMainWindow* window)
 {
-  //Don't worry about this function... just getting a image and setting it to the background.
-  QDir dir(QCoreApplication::applicationDirPath());
-  dir.cdUp();
-  dir.cdUp();
-  QString path = dir.path() +  QDir::separator() + "images" + QDir::separator() + "background.JPG";
-  QPixmap background(path);
-  background = background.scaled(window->size(), Qt::IgnoreAspectRatio);
-  QPalette palette;
-  palette.setBrush(QPalette::Background, background);
-  window->setPalette(palette);
+	//Don't worry about this function... just getting a image and setting it to the background.
+	QDir dir(QCoreApplication::applicationDirPath());
+	dir.cdUp();
+	dir.cdUp();
+	QString path = dir.path() +  QDir::separator() + "images" + QDir::separator() + "background.JPG";
+	QPixmap background(path);
+	background = background.scaled(window->size(), Qt::IgnoreAspectRatio);
+	QPalette palette;
+	palette.setBrush(QPalette::Background, background);
+	window->setPalette(palette);
+}
+
+void ISE::createPlayerLayout(QHBoxLayout *c)
+{
+	
+	QHBoxLayout *playerLayout = new QHBoxLayout();
+
+	//adding the player score layout to the container
+	c->addLayout(playerLayout);
+
+	
+	//added first player label & adding it to the playerLayout
+	firstLabel = new QLabel();
+	firstLabel->setText("Player 1: " + QString::number(firstScore));
+	playerLayout->addWidget(firstLabel);
+
+	//adding second player label & adding to playerLayout
+	secondLabel = new QLabel();
+	secondLabel->setText("Player 2: "+QString::number(secondScore));
+	playerLayout->addWidget(secondLabel);
+
+}
+
+void ISE::createGameLayout(QHBoxLayout *x)
+{
+	//Creating a new layout for our Game
+	QHBoxLayout *gameLayout = new QHBoxLayout();
+
+	//adding the game's layout to the container
+	x->addLayout(gameLayout);
+
+	for (int x=0; x<9; x++)
+	{
+		QToolButton *button = new QToolButton();
+		button->setText("Button: " +QString::number(x));
+		button->setToolButtonStyle(Qt::ToolButtonStyle::ToolButtonTextUnderIcon);
+		gameLayout->addWidget(button);
+	}
+
+
 }
 
 void ISE::initialize(QApplication *application)
 {
-  //Create the window screen for the program
-  QMainWindow *window = new QMainWindow();
-  window->resize(2200,1600);
-  window->showMaximized();
-  setBackground(window);
-  
-  //Create the 2d Renderer for ISE
-  GameViewer *viewer = new GameViewer(window);
-  window->setCentralWidget(viewer);
+	
+	//Create the window screen for the program
+	window = new QMainWindow();
+	window->resize(1600,1200);
+	window->showMaximized();
+	
+	//creating widget called container to hold game&score
+	QWidget *container = new QWidget();
 
-  // Adding some dock widgets to the window. Is for fun.
-  QDockWidget *widget;
-  for(int i = 0; i < 4; i++)
-  {
-    widget = new QDockWidget(0);
-    widget->setWindowTitle("DockWidget: " + QString::number(i));
-    window->addDockWidget(Qt::RightDockWidgetArea, widget);
-  }
+	//adding the larger container to the dock widget window
+	window->setCentralWidget(container);
+	QHBoxLayout *mainLayout = new QHBoxLayout();
+	container->setLayout(mainLayout);
+
+	createPlayerLayout(mainLayout);
+	createGameLayout(mainLayout);
+
+
 }
 ISEEnd
--- src/core/ISE/ISE.h
+++ src/core/ISE/ISE.h
@@ -3,6 +3,12 @@
 #include "ISEExports.h"
 #include "qapplication.h"
 #include "qobject.h"
+#include "qlabel.h"
+#include "qmainwindow.h"
+#include "qtoolbutton.h"
+#include "qlayout.h"
+
+
 
 ISEBegin
 class ISEAPI ISE
@@ -16,5 +22,21 @@ public:
 
   /** Initialize ISE! */
   void initialize(QApplication *application);
+
+private: 
+	void createPlayerLayout(QHBoxLayout *widget);
+
+	void createGameLayout(QHBoxLayout *widget);
+
+	QList<QToolButton*> buttons;
+
+	QLabel *firstLabel, *secondLabel;
+
+	QMainWindow *window;
+
+	int firstScore, secondScore;
+
+
+
 };
 ISEEnd
